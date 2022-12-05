f925ef6ff91713288578cba8b3a41ad7b4dee48c
Catherine Hardy <83373032+catehardy@users.noreply.github.com>
2022-12-05 10:58:56 +0000

    completed part 1

--- Day-3/AOC_03.js
+++ Day-3/AOC_03.js
@@ -1,24 +1,6 @@
 // Rucksack Reorganization: https://adventofcode.com/2022/day/3
-
-// A given rucksack always has the same number of items in each of its two compartments,
-// so the first half of the characters represent items in the first compartment,
-// while the second half of the characters represent items in the second compartment.
-// Every item type is identified by a single lowercase or uppercase letter
-// (that is, a and A refer to different types of items).
-
-// To help prioritize item rearrangement, every item type can be converted to a priority:
-// Lowercase item types a through z have priorities 1 through 26.
-// Uppercase item types A through Z have priorities 27 through 52.
-
 //Find the item type that appears in both compartments of each rucksack.
 // What is the sum of the priorities of those item types?
-
-// TODO:
-// DONE: split each rucksack into two sub-arrays
-// DONE: find the duplicates (items which occur in both halves) for each rucksack
-// DONE: push duplicates to duplicateItems array
-// convert/lookup value of each letter (see CS50 code)
-// sum total (reduce)
 
 const { Console } = require("console");
 const fs = require("fs");
@@ -27,27 +9,45 @@ const input = fs.readFileSync("./Day-3/input.txt", { encoding: "utf-8" });
 const rucksackArray = input.split("\n");
 let duplicateAdded = false;
 
-function findDuplicates() {
-  let splitRucksackArray = [];
+// split each rucksack into two sub-arrays
+function splitRucksacks() {
+  const splitRucksackArray = [];
   rucksackArray.map((rucksack) => {
     splitRucksackArray.push(rucksack.substring(0, rucksack.length / 2));
     splitRucksackArray.push(rucksack.substring(rucksack.length / 2));
   });
-  for (let i = 0; i < splitRucksackArray.length; i = i + 2) {
-    // console.log("part 1: ", splitRucksackArray[i], "part 2: ", splitRucksackArray[i + 1],"\n")
-    const duplicates = [];
+  findDuplicates(splitRucksackArray);
+}
+
+// find the duplicates (items which occur in both halves) for each rucksack
+// push duplicates to new array
+function findDuplicates(arr) {
+  const duplicates = [];
+  for (let i = 0; i < arr.length; i = i + 2) {
     duplicateAdded = false;
-    for (let j = 0; j < splitRucksackArray[i].length; j++) {
-      for (let k = 0; k < splitRucksackArray[i].length; k++) {
-        if (splitRucksackArray[i][j] === splitRucksackArray[i + 1][k] && duplicateAdded === false) {
-          duplicates.push(splitRucksackArray[i][j]);
+    for (let j = 0; j < arr[i].length; j++) {
+      for (let k = 0; k < arr[i].length; k++) {
+        if (arr[i][j] === arr[i + 1][k] && duplicateAdded === false) {
+          duplicates.push(arr[i][j]);
           // now stop looking for duplicates
           duplicateAdded = true;
         }
       }
     }
-    console.log(duplicates);
   }
+  getPriorityVal(duplicates);
 }
 
-findDuplicates(rucksackArray);
+// lookup value of each letter
+// sum total (reduce)
+function getPriorityVal(duplicates) {
+  const priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const indexOfVals = duplicates.map((item) => priorities.indexOf(item));
+  const priorityVals = indexOfVals.map((x) => x + 1);
+  const sumPriority = priorityVals.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+  console.log({ sumPriority });
+}
+
+splitRucksacks(rucksackArray);
